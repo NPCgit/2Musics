1f4b67f17c4dcb0a9a2642b92e5c567d490857aa
NPCgit <119781990+NPCgit@users.noreply.github.com>
2024-04-15 18:38:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-Tutorial = ""
+Music1 = ""
 Music2 = ""
+
+
+//wrist coords are numbers
+leftWristX = 0
+leftWristY = 0
+rightWristX = 0
+rightWristY = 0
+
 
 function preload()
 {
-    Tutorial = loadSound("YT tutorial music.mp3")
+    Music1 = loadSound("MusiK.mp3")
     Music2 = loadSound("Musik2.mp3")
 }
 
@@ -14,14 +22,42 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    //poseNet again
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet initialized!")
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log(leftWristX + " = LeftWristX " + leftWristY + " = LeftWristY");
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log(rightWristX + " = rightWristX " + rightWristY + " = rightWristY");
+    }
 }
 
 function draw()
 {
-    image(video, 0, 0, 600, 500)
+    image(video, 0, 0, 600, 500); //lua doesnt require semicolons, but JS does
 }
 
 function play()
 {
    // song.play();
+
+    song.setVolume(1)
+    //1 Is normal, like on pretty much everything but percentages
+    song.setRate(1)
+    //Speedhax lol spaceuk
 }
